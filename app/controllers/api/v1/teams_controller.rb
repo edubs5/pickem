514a5da4315602741385a649eead3e18bcbdac54
jsantacruz <juan@whitearrow.com>
2015-09-16 22:27:24 -0700

    Got the rails portion to respond to json request. Created a simple teams table to demonstrate the RESTful request. GET and SHOW works.

---
+++ app/controllers/api/v1/teams_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::TeamsController < Api::V1::BaseController
+
+  def index
+    # teams
+    render json: teams
+  end
+
+  def show
+    render json: team
+  end
+
+  def create
+    team = teams.create(teams_params)
+    if team.save
+      render json: team
+    else
+      render json: { error: 'Internal Server Error'},
+                    status: :internal_server_error
+    end
+  end
+
+  private
+    def teams
+      @teams ||= Team.all
+    end
+
+    def team
+      @team ||= teams.find(params[:id])
+    end
+
+    def teams_params
+      params.permit(:name)
+    end
+end
+
