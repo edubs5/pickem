c2fa99738b5984bb8311b1fe7ab3e340c72b0ad6
jsantacruz <juan@whitearrow.com>
2015-09-21 13:56:32 -0700

    Space fix.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1, :defaults => {format: "json"} do
+    namespace :v1, defaults: {format: "json"} do
       resources :teams, only: [:index, :show]
     end
   end
