f988a4308f6de0b4100b8b85207ab4193dc66378
jsantacruz <juan@whitearrow.com>
2015-09-21 13:27:30 -0700

    indentation fix?

--- db/migrate/20150917001346_create_teams.rb
+++ db/migrate/20150917001346_create_teams.rb
@@ -1,11 +1,11 @@
 class CreateTeams < ActiveRecord::Migration
   def change
     create_table :teams do |t|
-    	t.string :name
-      t.string :short_name
-      t.string :code
+	    	t.string :name
+	      t.string :short_name
+	      t.string :code
 
-      t.timestamps null: false
+	      t.timestamps null: false
     end
   end
 end
